aa03914c04e28b9cd8ae8d051a4fd74141aa71f0
SyrtcevVadim <syrtcevvi@gmail.com>
2021-05-13 00:07:30 +0300

    Removing of extra files

--- symUtilities.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<string>
-
-using std::string;
-/// Deletes begining and trailing spaces from a provided string
-string trim(string str);
-
-string numberToString(double number);
-double stringToNumber(string str);
